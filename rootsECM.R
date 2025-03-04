dc2b3214930ca2fa0bd3116fe3cc1ee40e0e7cc9
mykhanh0504 <mykhanhtonnu54@gmail.com>
2025-03-04 13:23:29 -0500

    plots and stats for soil n content and root ecm pct

---
+++ rootsECM.R
@@ -0,0 +1,86 @@
+library(tidyverse)
+library(dplyr)
+library(ggplot2)
+library(gt)
+library(car)
+library(multcomp)
+library(rstatix)
+library(kableExtra)
+
+ecm <- read_csv("rootsECM.csv")
+
+#Add Treatment & Pair columns
+ecm <- ecm %>% mutate(Treatment=case_when(
+  Unit %in% c("3/16","5/15","28/2","12/2") ~ "Shelterwood",
+  Unit %in% c("20/2","3/1") ~ "Seedtree",
+  Unit %in% c("44B","45C","46B","46C") ~ "Clearcut"))
+
+ecm <- ecm %>% replace_na(list(
+  Unit="Unnamed",Treatment="Unmanaged"))
+
+ecm <- ecm %>% mutate(Pair=case_when(
+  Unit %in% c("3/16","5/15")~"1",
+  Unit %in% c("20/2","3/1")~"2",
+  Unit %in% c("28/2","12/2")~"3",
+  Unit=="Unnamed"~"4"))
+
+ecm <- ecm %>% 
+  mutate(Unit=as.factor(Unit),
+         Disturbance=as.factor(Disturbance),
+         Treatment=as.factor(Treatment),
+         Pair=as.factor(Pair))
+
+ecm <- ecm %>% mutate(pct_colonized=colonized_tips/(colonized_tips+uncolonized_tips)*100)
+
+ecmb <- ecm %>% filter(Disturbance=="B")
+stats_ecmb <- get_summary_stats(ecmb)
+stats_ecmb <- stats_ecmb %>% mutate(Disturbance="B")
+stats_ecmb <- stats_ecmb %>% 
+  select(Disturbance,variable,min,max,median,mean,sd,se) %>% 
+  filter(variable %in% c("Age","Height_cm","Extension_growth_cm","DRC_mm",
+                         "nleaves","nlive_branches","ndead_branches",
+                         "Herbivory_pct","Pathogen_damage_pct","pct_colonized"))
+
+ecmc <- ecm %>% filter(Disturbance=="C")
+stats_ecmc <- get_summary_stats(ecmc)
+stats_ecmc <- stats_ecmc %>% mutate(Disturbance="C")
+stats_ecmc <- stats_ecmc %>% 
+  select(Disturbance,variable,min,max,median,mean,sd,se) %>% 
+  filter(variable %in% c("Age","Height_cm","Extension_growth_cm","DRC_mm",
+                         "nleaves","nlive_branches","ndead_branches",
+                         "Herbivory_pct","Pathogen_damage_pct","pct_colonized"))
+
+stats_ecm <- stats_ecmb %>% full_join(stats_ecmc)
+stats_ecm %>% kable(caption="Summarized statistics of 
+                     Q. rubra seedling measurements in 2024.") %>% 
+  kable_styling(html_font="Arial",
+                bootstrap_options = c("striped", "hover","condensed", "responsive"))
+
+ecm %>% 
+  ggplot(aes(x=Pair,y=pct_colonized,fill=Disturbance))+
+  geom_rect(aes(fill=Pair),alpha=0.5,
+            xmin=-Inf, xmax=Inf, ymin=-Inf, ymax=Inf)+
+  geom_boxplot(width=0.6,position=position_dodge(width=0.7))+
+  scale_fill_manual(values=c(p6,bc))+
+  facet_grid(~Pair,scales='free_x')+
+  theme_minimal()+
+  ylab("% colonized")+
+  labs(fill="Pairs")
+
+aov_ecm <- aov(pct_colonized~Disturbance+Pair,data=ecm)
+summary(aov_ecm)
+
+ecm %>% 
+  ggplot(aes(x=Height_cm,y=pct_colonized))+
+  geom_rect(aes(fill=Pair),alpha=0.5,
+            xmin=-Inf, xmax=Inf, ymin=-Inf, ymax=Inf)+
+  geom_point(aes(color=Disturbance))+
+  geom_smooth(method='lm')+
+  scale_fill_manual(values=c(p6,bc))+
+  facet_wrap(vars(Pair),scales='free')+
+  xlab("Height")+
+  ylab("pct colonized")
+
+ggplot(data=ecm,aes(x=Height_cm,y=pct_colonized))+
+  geom_point()+
+  geom_smooth(method='lm')
---
+++ soiln.R
@@ -0,0 +1,74 @@
+library(tidyverse)
+library(dplyr)
+library(ggplot2)
+library(gt)
+library(car)
+library(multcomp)
+library(rstatix)
+
+soiln <- read_csv("soilN.csv")
+
+soiln <- soiln %>% filter(Pair %in% c("2","3","4","5","6","8"))
+soiln <- soiln[-c(2),]
+
+soiln2 <- soiln %>% mutate(Cut=case_when(Stand %in% c("3/16","5/15","28/2","12/2") 
+                                           ~ "Shelterwood",
+                                           Stand %in% c("20/2","3/1") 
+                                           ~ "Seedtree",
+                                           Stand %in% c("44","46N","46") 
+                                           ~ "Clearcut"))
+
+soiln2 <- soiln2 %>% replace_na(list(Stand="Unnamed",Cut="Unmanaged"))
+
+soiln2 <- soiln2 %>% mutate(Pair2=case_when(Stand %in% c("3/16","5/15")~"1",
+                                       Stand %in% c("20/2","3/1")~"2",
+                                       Stand %in% c("28/2","12/2")~"3",
+                                       Stand=="Unnamed"~"4",
+                                       Stand %in% c("44","46N")~"5",
+                                       Stand=="46"~"6"))
+
+soiln2 <- soiln2 %>% select(Site,Stand,Treatment,
+                            NO3_mgkg,NH4_mgkg,Cut,Pair2,OM_pct)
+
+soiln2 <- soiln2 %>% 
+  mutate(Pair2=as.factor(Pair2),
+         Site=as.factor(Site),
+         Treatment=as.factor(Treatment),
+         Cut=as.factor(Cut))
+
+bc <- c("B"="#CC6677","C"="#88CCEE")
+p6 <- c("1"="#FBE3D6","2"="#FFFFCC","3"="#E8E8E8",
+        "4"="#C2F1C8","5"="#DCEAF7","6"="#DCEAF7")
+
+#totN, N/OMpct
+soiln2 <-  soiln2 %>% mutate(TotN=NO3_mgkg+NH4_mgkg,
+                             NO3_OM=NO3_mgkg/(OM_pct/100),
+                             NH4_OM=NH4_mgkg/(OM_pct/100),
+                             TotN_OM=NO3_OM+NH4_OM)
+
+#NO3 graph
+soiln2 %>% 
+  ggplot(aes(x=Pair2,y=NO3_OM,fill=Treatment))+
+  geom_rect(aes(fill=Pair2),alpha=0.5,
+            xmin=-Inf, xmax=Inf, ymin=-Inf, ymax=Inf)+
+  geom_col(width=0.6,position=position_dodge(width=0.7))+
+  scale_fill_manual(values=c(p6,bc))+
+  facet_grid(~Pair2,scales='free_x')+
+  theme_minimal()+
+  xlab("Pair")+
+  ylab("NO3 (mg/kg soil)")+
+  labs(fill="Pairs")
+
+#NH4 graph
+soiln2 %>% 
+  ggplot(aes(x=Site,y=NH4_OM,fill=Treatment))+
+  geom_rect(aes(fill=Pair2),alpha=0.5,
+            xmin=-Inf, xmax=Inf, ymin=-Inf, ymax=Inf)+
+  geom_col(width=0.6,position=position_dodge(width=0.7))+
+  scale_fill_manual(values=c(p6,bc))+
+  facet_grid(~Pair2,scales='free_x')+
+  theme_minimal()+
+  xlab("Pair")+
+  ylab("NH4 (mg/kg soil)")+
+  labs(fill="Pairs")
+
