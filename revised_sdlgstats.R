b229e95bde19f0f91de8d2043a04c02dc5a72207
Khanh Ton <Khanh.Ton@jonesctr.org>
2025-11-17 13:23:31 -0500

    i believe i figured out the fixed v random effects

--- revised_sdlgstats.R
+++ revised_sdlgstats.R
@@ -42,11 +42,11 @@ count23x <- count23x %>%
 count23x$Plot_unique <- with(count23x,paste(Location, Unit, Transect, Plot, sep="_"))
 count23x$Plot_unique <- as.factor(count23x$Plot_unique)
 
-df <- count23x %>% filter(!Location %in% c("BEF"))
+df <- count23x %>% filter(!Location %in% c("BEF","CF"))
 
 # ANOVA count -------------------------------------------------------------
 
-aov_count <- aov(OAKcount~Disturbance+Error(Unit),data=df)
+aov_count <- aov(OAKcount~Disturbance+Pair+Error(Unit),data=df)
 summary(aov_count)
 
 # Clean up 2023 df --------------------------------------------------------
@@ -68,15 +68,15 @@ ms23x <- ms23x %>% replace_na(list(
   Unit="Unnamed",Treatment="Unmanaged"))
 
 ms23x <- ms23x %>% mutate(Unit=ifelse(Unit=="Unnamed" & Disturbance =="B","Unnamed_B",
-                                            ifelse(Unit=="Unnamed" & Disturbance == "C", "Unnamed_C",Unit)))
+                                            ifelse(Unit=="Unnamed" & 
+                                                     Disturbance == "C", "Unnamed_C",
+                                                   Unit)))
 
 ms23x <- ms23x %>% mutate(Pair=case_when(
   Unit %in% c("3/16","5/15")~"1",
   Unit %in% c("20/2","20/3","20/4","20/5","3/1")~"2",
   Unit %in% c("28/2","28/3","12/2")~"3",
-  Unit %in% c("Unnamed_B","Unnamed_C")~"4",
-  Unit %in% c("44B","45C")~"5",
-  Unit %in% c("46B","46C")~"6"))
+  Unit %in% c("Unnamed_B","Unnamed_C")~"4"))
 
 ms23x <- ms23x %>% 
   mutate(Unit=as.factor(Unit),
@@ -93,11 +93,10 @@ ms23x$Plot_unique <- as.factor(ms23x$Plot_unique)
 
 # ANOVA DRC ---------------------------------------------------------------
 
-aov_drc23 <- aov(DRC_mm~Disturbance+Error(Unit/Plot_unique),data=ms23x)
+aov_drc23 <- aov(DRC_mm~Disturbance+Pair+Error(Unit/Plot_unique),data=ms23x)
 summary(aov_drc23)
 
 # Clean up 2024 df --------------------------------------------------------
-
 ms24 <- read_csv("oakmsms24.csv")
 
 ms24 <- ms24 %>% mutate(Location=as.factor(Location)) %>% 
@@ -115,12 +114,14 @@ ms24x <- ms24x %>% replace_na(list(
   Unit="Unnamed",Treatment="Unmanaged"))
 
 ms24x <- ms24x %>% mutate(Unit=ifelse(Unit=="Unnamed" & Disturbance =="B","Unnamed_B",
-                                            ifelse(Unit=="Unnamed" & Disturbance == "C", "Unnamed_C",Unit)))
+                                      ifelse(Unit=="Unnamed" & 
+                                               Disturbance == "C", "Unnamed_C",
+                                             Unit)))
 
 ms24x <- ms24x %>% mutate(Pair=case_when(
   Unit %in% c("3/16","5/15")~"1",
-  Unit %in% c("20/2","20/3","20/4","20/5","3/1")~"2",
-  Unit %in% c("28/2","28/3","12/2")~"3",
+  Unit %in% c("20/2","3/1")~"2",
+  Unit %in% c("28/2","12/2")~"3",
   Unit %in% c("Unnamed_B","Unnamed_C")~"4"))
 
 ms24x <- ms24x %>% 
@@ -141,31 +142,32 @@ ms24x$nleaves <- replace(ms24x$nleaves,ms24x$nleaves=="100+","100") %>%
 ms24x$Plot_unique <- with(ms24x,paste(Location, Unit, Transect, Plot, sep="_"))
 ms24x$Plot_unique <- as.factor(ms24x$Plot_unique)
 
+ms24x <- ms24x %>% filter(Survey=="2")
+
 # ANOVA XG nleaves --------------------------------------------------------
 
-aov_xg24 <- aov(Extension_growth_cm~Disturbance+Error(Unit/Plot_unique),data=ms24x)
+aov_xg24 <- aov(Extension_growth_cm~Disturbance+Pair+Error(Unit),data=ms24x)
 summary(aov_xg24)
 
-aov_leaves24 <- aov(nleaves~Disturbance+Error(Unit/Plot_unique),data=ms24x)
+aov_leaves24 <- aov(nleaves~Disturbance+Error(Unit),data=ms24x)
 summary(aov_leaves24)
 
 # LMMs --------------------------------------------------------------------
 
 #sdlg count
 library(glmmTMB)
-m_count <- glmmTMB(OAKcount~Disturbance+(1|Pair/Unit),
+m_count <- glmmTMB(OAKcount~Disturbance+Pair+(1|Unit),
                 ziformula=~1,family=nbinom2,data=df)
 summary(m_count)
 
 library(emmeans)
-emm <- emmeans(m_count,pairwise~Disturbance,type="response")
-emm
-plot(emm)
+emmeans(m_count,pairwise~Disturbance,type="response")
+plot(emmeans(m_count,pairwise~Disturbance,type="response"))
 
-#drc 2023 HAVE TO FIGURE OUT THE CORRECT ERROR TERM
+#drc 2023 
 library(lme4)
 ms23x$logDRC <- log(ms23x$DRC_mm)
-m_drc <- lmer(logDRC ~ Disturbance+(1|Pair/Plot_unique),data=ms23x)
+m_drc <- lmer(logDRC ~ Disturbance+Pair+(1|Unit/Plot_unique),data=ms23x)
 summary(m_drc)
 
 #back transform
@@ -173,3 +175,34 @@ emmeans(m_drc,~Disturbance,type="response")
 #pairwise contrast
 emmeans(m_drc,pairwise~Disturbance,type="response")
 plot(emmeans(m_drc, ~ Disturbance, type = "response"))
+
+#drc 2024
+ms24x$logDRC <- log(ms24x$DRC_mm)
+m_drc2 <- lmer(logDRC ~ Disturbance+Pair+(1|Unit/Plot_unique),data=ms24x)
+summary(m_drc2)
+
+#back transform
+emmeans(m_drc2,~Disturbance,type="response")
+#pairwise contrast
+emmeans(m_drc2,pairwise~Disturbance,type="response")
+plot(emmeans(m_drc2, ~ Disturbance, type = "response"))
+
+#xg 2024
+ms24x$logXG <- log(ms24x$Extension_growth_cm+1)
+m_xg <- lmer(logXG ~ Disturbance+Pair+(1|Unit/Plot_unique),data=ms24x)
+summary(m_xg)
+
+#back transform
+emm_xg <- emmeans(m_xg,~Disturbance,type="response")
+emm_xg
+#pairwise contrast
+emmeans(m_xg,pairwise~Disturbance,type="response")
+plot(emmeans(m_xg, ~ Disturbance, type = "response"))
+
+#nleaves 2024
+m_leaves <- glmmTMB(nleaves~Disturbance+Pair+(1|Unit/Plot_unique),
+                    family=nbinom2, data=ms24x)
+summary(m_leaves)
+
+emmeans(m_leaves,pairwise~Disturbance,type="response")
+plot(emmeans(m_leaves,pairwise~Disturbance,type="response"))
